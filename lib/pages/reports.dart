219b01f44e60dfa72a7b6269d773c177a92f0488
Gilbert John Gallega <gilbertjohngallega82@gmail.com>
2026-02-08 08:59:56 +0800

    Transferred the chart into a different file

--- lib/pages/reports.dart
+++ lib/pages/reports.dart
@@ -1,11 +1,11 @@
-import 'dart:io';
+
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:pos_app/db/reports.dart';
 import 'package:pos_app/widgets/footer.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pos_app/utils/responsive.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:pos_app/widgets/sale_chart.dart';
 
 class Reports extends StatefulWidget {
   const Reports({super.key});
@@ -34,9 +34,6 @@ void initState() {
   loadReportsCard();
   loadSalesTrend();
 }
-
-
-
 
 
 
@@ -351,148 +348,11 @@ String _valueOrLoading(String key, {bool peso = false}) {
                         ),)
                         :salesTrend.isEmpty
                         ? Center(child: Text("No data", style: GoogleFonts.kameron()))
-                        : Padding(
-                            padding: const EdgeInsets.fromLTRB(8,20,8,10),
-                            child: LineChart(
-                              LineChartData(
-                                gridData: FlGridData(show: true),
-                                titlesData: FlTitlesData(
-                                  leftTitles: AxisTitles(
-                                    sideTitles: SideTitles(
-                                      showTitles: true,
-                                      reservedSize: 35,
-                                      getTitlesWidget: (value, meta) {
-                                      return Text(
-                                        '${value.toStringAsFixed(0)}',
-                                        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 12),
-                                      );
-                                    },),
-                                    
-                                  ),
-                                  rightTitles: AxisTitles(
-                                    sideTitles: SideTitles(
-                                      showTitles: true,
-                                      reservedSize: 28,
-                                      getTitlesWidget: (value, meta) {
-                                      return Text(
-                                        '${value.toStringAsFixed(0)}',
-                                        style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500, fontSize: 12),
-                                      );
-                                    },
-                                    ),
-                                  ),topTitles: AxisTitles(
-                                    sideTitles: SideTitles(
-                                      showTitles: false,
-                                      reservedSize: 28,
-                                    ),
-                                  ),
-                                  bottomTitles: AxisTitles(
-                                    sideTitles: SideTitles(
-                                      showTitles: true,
-                                      interval: 1,
-                                      getTitlesWidget: (value, meta) {
-                                        int index = value.toInt();
-                                        if (index >= 0 && index < salesTrend.length) {
-                                          final date = DateFormat('MM/dd/yy').format(
-                                              DateTime.parse(salesTrend[index]['date']));
-                                          return Text(date, style: TextStyle(fontSize: 10));
-                                        }
-                                        return const Text('');
-                                      },
-                                    ),
-                                  ),
-                                ),
-                                borderData: FlBorderData(show: true),
-                                lineTouchData: LineTouchData(
-                                enabled: true,
-                                touchTooltipData: LineTouchTooltipData(
-                                  getTooltipColor: (spot) => Colors.white,
-                                  getTooltipItems: (touchedSpots) {
-                                    return touchedSpots.map((touchedSpot) {
-                                      // Only show tooltip for the first line (revenue)
-                                      if (touchedSpot.barIndex == 0) {
-                                        final index = touchedSpot.spotIndex;
-                                        final date = DateFormat('MM/dd').format(
-                                          DateTime.parse(salesTrend[index]['date'])
-                                        );
-                                        final revenue = salesTrend[index]['revenue'];
-                                        final totalSales = salesTrend[index]['totalSales'];
-                                        
-                                        return LineTooltipItem(
-                                          '$date\nRevenue: ₱${revenue.toStringAsFixed(2)}\nSales: $totalSales',
-                                          TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                                        );
-                                      } else {
-                                        // Return null for other lines to hide their tooltips
-                                        return null;
-                                      }
-                                    }).toList();
-                                  },
-                                ),
-                              ),
-                                lineBarsData: [
-                                  LineChartBarData(
-                                    spots: List.generate(
-                                      salesTrend.length,
-                                      (index) => FlSpot(
-                                        index.toDouble(),
-                                        salesTrend[index]['revenue'] as double,
-                                      ),
-                                    ),
-                                    isCurved: true, 
-                                    color: const Color.fromARGB(255, 14, 68, 161),
-                                    barWidth: 3,
-                                    dotData: FlDotData(show: true),
-                                     belowBarData: BarAreaData(
-                                      show: true,
-                                      color: const Color.fromARGB(100, 14, 68, 161), // color with opacity
-                                      gradient: LinearGradient(
-                                        colors: [
-                                          Color.fromARGB(150, 14, 68, 161), // top color
-                                          Color.fromARGB(0, 14, 68, 161),   // bottom color (fade)
-                                        ],
-                                        begin: Alignment.topCenter,
-                                        end: Alignment.bottomCenter,
-                                      ),
-                                    ),
-                                  ),
-                                   LineChartBarData(
-                                    spots: List.generate(
-                                      salesTrend.length,
-                                      (index) => FlSpot(  
-                                        index.toDouble(),
-                                        (salesTrend[index]['totalSales'] ?? 0).toDouble(),
-                                      ),
-                                    ),
-                                    isCurved: true, 
-                                    color: const Color.fromARGB(255, 255, 152, 0),
-                                    barWidth: 3,
-                                    dotData: FlDotData(show: true),
-                                     belowBarData: BarAreaData(
-                                      show: true,
-                                      color: const Color.fromARGB(100, 255, 153, 0), // color with opacity
-                                      gradient: LinearGradient(
-                                        colors: [
-                                          Color.fromARGB(160, 255, 153, 0), // top color
-                                          Color.fromARGB(0, 255, 153, 0),   // bottom color (fade)
-                                        ],
-                                        begin: Alignment.topCenter,
-                                        end: Alignment.bottomCenter,
-                                      ),
-                                    ),
-                                    
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                        : SaleChartWidget(salesTrend: salesTrend, isLoading: isSaleTrendLoading )
                     ),
                     ),
                   ],
                 )
-
-                
-
             ],
           ),
         ),
